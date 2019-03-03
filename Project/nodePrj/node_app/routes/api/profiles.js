d3f17e41aa20c523e4d585c99e3df11d2ede4fd7
gregrgr <my294976910@gmail.com>
2019-03-03 21:11:25 +0800

    update

--- Project/nodePrj/node_app/routes/api/profiles.js
+++ Project/nodePrj/node_app/routes/api/profiles.js
@@ -36,4 +36,80 @@ router.post(
     });
 });
 
+
+//$route  POST api/users/profiles
+//@desc   获取所有信息
+//@access private
+router.get(
+    "/",
+    passport.authenticate("jwt",{session:false}),(req,res)=>{
+        Profile.find()
+        .then(profile=>{
+            if(!profile){
+                return res.status(404).json('Nothing');
+            }
+            res.json(profile);
+        })
+        .catch(err=>{
+            res.status(404).json(err);
+        })
+    }
+)
+
+//$route  POST api/users/:id
+//@desc   获取单条信息
+//@access private
+router.get(
+    "/:id",
+    passport.authenticate("jwt",{session:false}),(req,res)=>{
+        Profile.find({_id:req.params.id})
+        .then(profile=>{
+            if(!profile){
+                return res.status(404).json('Nothing');
+            }
+            res.json(profile);
+        })
+        .catch(err=>{
+            res.status(404).json(err);
+        })
+    }
+)
+
+//$route  POST api/users/edit
+//@desc   修改单条信息
+//@access private
+router.post(
+    "/edit/:id",
+    passport.authenticate("jwt",{session:false}),(req,res)=>{
+    const profileFields = {};
+    if(req.body.type)profileFields.type = req.body.type;
+    if(req.body.describe)profileFields.describe = req.body.describe;
+    if(req.body.income)profileFields.income = req.body.income;
+    if(req.body.expend)profileFields.expend = req.body.expend;
+    if(req.body.cash)profileFields.cash = req.body.cash;
+    if(req.body.remark)profileFields.remark = req.body.remark;
+
+    Profile.findOneAndUpdate(
+        {_id:req.params.id},
+        {$set:profileFields},
+        {new:true}
+    ).then(profile=>{
+        res.json(profile)
+    })
+});
+
+//$route  POST api/users/delet
+//@desc   删除单条信息
+//@access private
+router.delete(
+    "/delete/:id",
+    passport.authenticate("jwt",{session:false}),(req,res)=>{
+
+    Profile.findOneAndRemove(
+        {_id:req.params.id}
+    ).then(profile=>{
+        profile.save().then(profile=>res.json(profile))
+    }).catch(err=>res.status(404).json("删除失败"))
+});
+
 module.exports = router;
